32752c0b785deb40a8cfe89d05bf85118b475fec
couch-potahto <39213650+couch-potahto@users.noreply.github.com>
2019-03-11 22:56:04 +0800

    Update settings.py

--- athlete_management/settings.py
+++ athlete_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o!*74y(t__#wr!8%$5%+)&msl(*6#s%ului@bsv7yqjjohywpx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
