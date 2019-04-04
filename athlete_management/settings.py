e1d1db1e541120cb9095d656f3ba23f1fd7c24ec
couch-potahto <39213650+couch-potahto@users.noreply.github.com>
2019-04-04 13:49:56 +0800

    Update settings.py

--- athlete_management/settings.py
+++ athlete_management/settings.py
@@ -109,7 +109,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'agogestrengthapp@gmail.com'
+EMAIL_HOST_PASSWORD = 'fkingnerds'
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
